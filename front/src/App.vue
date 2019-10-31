af7ff31f8c0c3ec2f035d91f64f75f0f497fe5a9
Daniel <mr.devilstar@gmail.com>
2019-10-31 14:57:33 +0200

    grid fix

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,25 @@
 <template>
-    <div id="app" class="max-w-screen-xl w-full mx-auto px-6">
+    <div id="app">
         <global-header></global-header>
+        <div class="flex max-w-screen-xl w-full mx-auto px-6">
+            <div class="w-1/5">
+                <sidebar />
+            </div>
+            <div class="w-3/5 pl-6 pr-6">
+                2
+            </div>
+            <div class="w-1/5">
+                3
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import GlobalHeader from "./components/global/header/Header";
+    import Sidebar from "./components/global/sidebar/Sidebar";
     export default {
-        components: {GlobalHeader}
+        components: {Sidebar, GlobalHeader}
     }
 </script>
 
