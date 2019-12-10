536ab5029960186e7837d014adc1437f3ab49f7f
Daniel <mr.devilstar@gmail.com>
2019-12-10 17:49:21 +0200

    wip - comments

---
+++ front/src/views/posts/PostCommentLevel.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="">
+        <template v-for="item in level">
+            <div class="pl-3">
+                {{ item.author }}
+                <template v-if="item.children.length">
+                    <post-comment-level :level="item.children" />
+                </template>
+            </div>
+        </template>
+<!--        {{ level.author }}-->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PostCommentLevel",
+        props: [
+            'level',
+        ],
+    }
+</script>
+
+<style scoped>
+
+</style>
--- front/src/views/posts/PostComments.vue
+++ front/src/views/posts/PostComments.vue
@@ -1,12 +1,76 @@
 <template>
     <div>
-        123
+        <div class="flex items-center px-3 py-2">
+            <span class="text-lg font-semibold text-gray-700">
+                500 комментариев
+            </span>
+        </div>
+
+        <div class="flex items-center px-3 pt-2 border-b">
+            <ul class="flex text-sm font-semibold">
+                <li class="mr-3 border-b-2 pb-2 border-purple-600">
+                    Популярные
+                </li>
+                <li class="border-b-2 border-transparent hover:border-b-2 hover:border-purple-600 cursor-pointer">
+                    По порядку
+                </li>
+            </ul>
+        </div>
+
+        <div class="flex items-center px-3 py-2">
+            <post-comment-level :level="data" />
+        </div>
     </div>
 </template>
 
 <script>
+    import PostCommentLevel from "./PostCommentLevel";
     export default {
-        name: "PostComments"
+        name: "PostComments",
+        components: {PostCommentLevel},
+        data() {
+            return {
+                data: [
+                    {
+                        id: 1,
+                        author: 'Ник Автора 1',
+                        date: '2019-12-10 07:56:40',
+                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                        depth: 0,
+                        children: [
+                            {
+                                id: 3,
+                                author: 'Ник Автора 2',
+                                date: '2019-12-10 07:56:40',
+                                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                                depth: 1,
+                                children: [
+                                    {
+                                        id: 4,
+                                        author: 'Ник Автора 3',
+                                        date: '2019-12-10 07:56:40',
+                                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                                        depth: 2,
+                                        children: []
+                                    }
+                                ]
+                            }
+                        ],
+                    },
+                    {
+                        id: 2,
+                        author: 'Ник Автора 4',
+                        date: '2019-12-10 07:56:40',
+                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                        depth: 0,
+                        children: [],
+                    }
+                ]
+            }
+        },
+        methods: {
+
+        }
     }
 </script>
 
