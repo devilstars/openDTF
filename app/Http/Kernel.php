b7542b1b5a19a214459f43fb9e21ddb93b0fc1da
DevilStars <mr.devilstar@gmail.com>
2020-04-17 12:07:49 +0300

    laravel 6 to 7 update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Barryvdh\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
@@ -40,8 +40,8 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            'bindings',
-            \Barryvdh\Cors\HandleCors::class,
+            'bindings'
+//            \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
