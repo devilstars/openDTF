3b5970c5a16f2896a2f12502d4224a6e0724d66b
DevilStars <mr.devilstar@gmail.com>
2020-04-21 17:22:21 +0300

    laravel auth - wip

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,20 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\UserAuthRequest;
 use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function auth(Request $request) {
+    public function auth(UserAuthRequest $request) {
         try {
             $user = User::query()->where('email', $request->input('email'))->firstOrFail();
         } catch (\Exception $e) {
             $response = [
                 'status' => 404,
-                'message' => 'Пользователь не найден'
+                'message' => 'Пользователь с такой электронной почтой или паролем не найден'
             ];
-            return response($response, 500);
+            return response($response, 404);
         }
 
         $token = $user->createToken('token-name');
